0d16269b37e4c9b05f65e155a9d9b42c94ca3b0e
Lemsito22 <lemquilca22@gmail.com>
2026-01-09 12:44:15 +0100

    ...

--- RPG/src/carrito.java
+++ RPG/src/carrito.java
@@ -1,14 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class carrito {
+    public static int comprobarInt(){
+        Scanner sc = new Scanner(System.in);
+        int respuesta = 0;
+        boolean comprobar=true;
+        while (comprobar) {
+            try {
+                respuesta = sc.nextInt();
+            } catch (InputMismatchException e){
+                System.out.println("Error, los datos no son numericos, vuelve a intentarlo");
+                sc.next();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            comprobar=false;
+        }
+        return respuesta;
+    }
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String [] productos = {"Pan","Huevos","Aceite","Harina","Leche"};
-        int [] precio = {1,6,5,3,4};
-        int [] carrito = new int[5];
+        String [] productos = {"producto invalido","Pan","Huevos","Aceite","Harina","Leche"};
+        int [] precio = {0,1,6,5,3,4};
+        int [] carrito = new int[6];
         int respuesta;
 
         System.out.println("PRODUCTOS");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i < 6; i++) {
             String verproductos = productos[i];
             System.out.println(i+")"+verproductos+" "+precio[i]+"€");
         }
@@ -16,12 +34,9 @@ public class carrito {
         boolean añadirCarrito=true;
         while (añadirCarrito) {
             System.out.println("¿Qué producto desea añadir al carrito?");
-            respuesta = sc.nextInt();
+            respuesta=comprobarInt();
             //AÑADIR PRODUCTO AL CARRITO
             switch (respuesta) {
-                case 0:
-                    carrito[0] += 1;
-                    break;
                 case 1:
                     carrito[1] += 1;
                     break;
@@ -34,6 +49,9 @@ public class carrito {
                 case 4:
                     carrito[4] += 1;
                     break;
+                case 5:
+                    carrito[5] += 1;
+                    break;
             }
             System.out.println("Usted ha añadido 1 " + productos[respuesta] +" "+ " a su carrito");
             System.out.println("¿Desea añadir un producto más? (SI/NO)");
@@ -44,7 +62,7 @@ public class carrito {
             } else {
                 añadirCarrito=false;
                 System.out.println("TICKET");
-                for (int i = 0; i < 5; i++) {
+                for (int i = 1; i < 6; i++) {
                     int sumaprecio=0;
                     if (carrito[i]>0){
                         System.out.println(productos[i]+" x"+carrito[i]+" "+precio[i]+"€");
